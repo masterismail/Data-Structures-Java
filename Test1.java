76165cb893faa95616d1ba2952349c08996f0165
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-03-28 15:55:32 +0530

    my first commit

--- Test1.java
+++ Test1.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
 public class Test1 {
-    String word = "Ismail"; 
-		
-    for(int i=word.length()-1;i>=0;i--) {
-        System.out.print(word.charAt(i));
+    public static void main(String[] argument) {
+        String word = "Ismail";
 
-    
-}
+        for (int i = word.length() - 1; i >= 0; i--) {
+            System.out.print(word.charAt(i));
+
+
+        }
+    }
 }
